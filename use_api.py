078239ef78ac6b61c1986ec298f80b63bd00c2ba
조준희 <61267275+2runo@users.noreply.github.com>
2020-10-11 14:56:31 +0900

    Update use_api.py

--- use_api.py
+++ use_api.py
@@ -1,8 +1,8 @@
 import requests
 
-res = requests.get('http://158.247.212.202:8000/us-restaurant-menus', params={'lon': '39.787947', 'lat': '-86.160467'})
+res = requests.get('http://localhost:8000/us-restaurant-menus', params={'lon': '39.787947', 'lat': '-86.160467'})
 print(res.text)
 
 
-res = requests.get('http://158.247.212.202:8000/recipe_search_and_diet', params={'q': 'Egg Toast'})
+res = requests.get('http://localhost:8000/recipe_search_and_diet', params={'q': 'Egg Toast'})
 print(res.text)
